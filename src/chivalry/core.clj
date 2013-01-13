b9d53cec7768226c7ac8e30942d1da7a1934faab
Joshua Eckroth <eckroth@cse.ohio-state.edu>
2013-01-13 08:30:57 -0500

    Initial lein template.

---
+++ src/chivalry/core.clj
@@ -0,0 +1,6 @@
+(ns chivalry.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/chivalry/core_test.clj
@@ -0,0 +1,7 @@
+(ns chivalry.core-test
+  (:use clojure.test
+        chivalry.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
